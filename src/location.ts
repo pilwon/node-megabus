081d352fd2e8e6fbc2651321d96a30b4cd48ce7e
Jibran Kalia <jibran.kalia@gmail.com>
2018-06-16 16:32:58 -0500

    Added Dallas and Austin city codes (#2)

--- src/location.ts
+++ src/location.ts
@@ -1,25 +1,37 @@
 export enum LocationId {
   Atlanta = 289,
+  Austin = 320,
   Boston = 94,
   Chicago = 100,
+  Dallas = 317,
   Philadelphia = 127,
   Toronto = 145,
   NewHaven = 122,
   NewYork = 123,
-  Washington = 142,
+  Washington = 142
 }
 
 export const LocationIds = {
-  'Atlanta': LocationId.Atlanta,
-  'Boston': LocationId.Boston,
-  'Chicago': LocationId.Chicago,
-  'New Haven': LocationId.NewHaven,
-  'New York': LocationId.NewYork,
-  'Philadelphia': LocationId.Philadelphia,
-  'Toronto': LocationId.Toronto,
-  'Washington': LocationId.Washington,
+  Atlanta: LocationId.Atlanta,
+  Austin: LocationId.Austin,
+  Boston: LocationId.Boston,
+  Chicago: LocationId.Chicago,
+  Dallas: LocationId.Dallas,
+  "New Haven": LocationId.NewHaven,
+  "New York": LocationId.NewYork,
+  Philadelphia: LocationId.Philadelphia,
+  Toronto: LocationId.Toronto,
+  Washington: LocationId.Washington
 };
 
 export type LocationName =
-  'Atlanta' | 'Boston' | 'Chicago' | 'New Haven' | 'New York' |
-  'Philadelphia' | 'Toronto' | 'Washington';
+  | "Atlanta"
+  | "Austin"
+  | "Boston"
+  | "Chicago"
+  | "Dallas"
+  | "New Haven"
+  | "New York"
+  | "Philadelphia"
+  | "Toronto"
+  | "Washington";
